394d01ba202f618db7b6608793e46cd8c7544c54
Rana Nooris Ur Rehman <rananooris8@gmail.com>
2020-11-21 14:05:07 +0500

    login page done

---
+++ resources/views/footer/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="footer mt-auto py-3">
+    <div class="container">
+      <span class="text-muted ">created by @oggy-17</span>
+    </div>
+  </footer>
---
+++ resources/views/header/header.blade.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <a class="navbar-brand" href="#">GROCERY</a>
+      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <li class="nav-item">
+          <a class="nav-link" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Orders</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Disabled</a>
+        </li>
+
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </ul>
+
+      <ul class="nav navbar-nav navbar-right">
+          <li>
+              <a href="#">Add to Cart</a>
+            </li>
+      </ul>
+    </div>
+  </nav>
